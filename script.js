791c4f1369545755f261f13dde2c16c2f7b7cda9
Raul <raullmdsantos@gmail.com>
2026-02-17 16:38:31 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = document.getElementById("nome");
+let email = document.getElementById("email");
+let idade = document.getElementById("nome");
+let salvar = document.getElementById("salvar");
+let resul = document.getElementById("resul");
+
+salvar.addEventListener("click", adicionar);
+
+function adicionar() {
+  alert("test");
+}
